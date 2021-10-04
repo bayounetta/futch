fdc3d5327c21deb38437d579a763f17a05d553ad
juniper wilde <junewilde@pm.me>
2021-10-04 13:51:00 -0700

    remove the rest of deprecated folder

---
+++ pages/blog/index.md
@@ -0,0 +1,21 @@
+---
+layout: base
+---
+## Blog posts, baked with love!
+<!-- {posts.map(({ node }) => {
+  const title = node.frontmatter.title || node.fields.slug;
+  return (
+    <div className="post" key={node.fields.slug}>
+      <div className="postTitle">
+        <Link to={node.fields.slug}>{title}</Link>
+        <span> - </span>
+        <span className="postDate">{node.frontmatter.date}</span>
+      </div>
+      <div className="postExcerpt"
+        dangerouslySetInnerHTML={{
+          __html: node.frontmatter.description || node.excerpt,
+        }}
+      />
+    </div>
+  );
+})} -->
--- resources/layouts/base.11ty.js
+++ resources/layouts/base.11ty.js
@@ -8,14 +8,25 @@ exports.render = function(data) {
     <meta name="description" content="${ data.description || data.metadata.description }">
   </head>
   <body>
-    <header>
-      <h1 class="home"><a href="${ '/' || data.url }">${ data.title || data.metadata.title }</a></h1>
-    </header>
-    <main>
+    <main> <!-- maybe not needed? -->
+      <header>
+        <h1 class="home"><a href="${ '/' || data.url }">${ data.title || data.metadata.title }</a></h1>
+      </header>
       ${ data.content || data.metadata.safe }
+      <footer>
+        <small>© juniper wilde 2019-2021</small>
+      </footer>
     </main>
   </body>
 </html>`;
 };
 
-
+// <main>
+// <Sidebar/>
+// <section className="page">
+//   {children}
+//   <footer>
+//     <small>© juniper wilde 2019-2020</small>
+//   </footer>
+// </section>
+// </main>
